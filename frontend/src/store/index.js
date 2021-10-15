63c3a63b58b1cf3f5764dde779efce728dc4db4e
Micael Matheus <micael.matheus@live.com>
2021-10-15 13:36:51 -0300

    Centralizando state pacientes

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,14 +1,35 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import api from '@/api'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    // pacientes
+    pacientes: []
   },
   mutations: {
+    // pacientes
+    listarPacientes: (state, pacientes) => {
+      state.pacientes = pacientes
+    }
   },
   actions: {
+    // pacientes
+    buscarPacientesApi: async ({ commit, state }, params) => {
+      const { data } = await axios.get(`${api.apiURL}pacientes`)
+      commit('listarPacientes', data)
+      return data
+    },
+    deletarPaciente: async ({ commit, state }, params) => {
+      await axios.delete(`${api.apiURL}pacientes/${params}`)
+
+      const { data } = await axios.get(`${api.apiURL}pacientes`)
+      commit('listarPacientes', data)
+      console.log('state', state)
+    }
   },
   modules: {
   }
